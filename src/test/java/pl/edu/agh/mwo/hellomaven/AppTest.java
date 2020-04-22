9b778b9fc31747e7ccf956d3a7a4a3663f3d83cc
Wojciech Frącz <fraczwojciech@gmail.com>
2020-04-22 20:48:03 +0200

    Simplify project structure

--- src/test/java/pl/edu/agh/mwo/hellomaven/AppTest.java
+++ src/test/java/pl/edu/agh/mwo/hellomaven/AppTest.java
@@ -1,36 +1,15 @@
 package pl.edu.agh.mwo.hellomaven;
 
-import java.util.List;
-
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
-
-/**
- * Unit test for the App class.
- */
-@RunWith(MockitoJUnitRunner.class)
 public class AppTest {
 
     /**
-     * Insert here your mocks
-     */
-    @Mock
-    private List mockList;
-
-    /**
-     * Sample test with given-when-then structure using MockitoBDD and AssertJ
+     * Sample test.
      */
     @Test
-    public void listDoesWorkWithMocks() {
-        given(mockList.size()).willReturn(10);
-
-        final int sizeOfTheList = mockList.size();
-
-        assertThat(sizeOfTheList).isEqualTo(10);
+    public void sampleTest() {
+        Assert.assertTrue(true);
     }
 }
